bd57fd3db899f03664013192817e301854d00696
Pavlos Chrysochoidis <p.chrysochoidis@gmail.com>
2015-02-01 16:11:26 +0200

    Short url ajax in progress.

--- app/myUrlJ/myUrlUI/src/main/webapp/index.jsp
+++ app/myUrlJ/myUrlUI/src/main/webapp/index.jsp
@@ -1,13 +1,57 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
-
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
-</head>
-<body>
-HELLO FROM HERE!!!
-</body>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"> 
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" type="image/png" href="images/favicon.png">
+		<title>MicroUrl. Make your links shorter!</title>
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+		<script type="text/javascript"	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/home.js"></script>
+	</head>
+	<body>
+	<%
+	//TODO: remove
+	Thread.currentThread().sleep(2000);
+	%> 
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed"
+						data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span> <span
+							class="icon-bar"></span> <span class="icon-bar"></span> <span
+							class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="home">μUrl</a>
+				</div>
+				<div class="collapse navbar-collapse"
+					id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li><a href="home">Home</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<div class="container-fluid">
+		  <div class="row">
+		    <div class="jumbotron">
+			  <h1><label for="link_input">Enter your link:</label></h1>
+			  <p>
+			 	  <div class="form-group">
+				    <input type="text" class="form-control" id="link_input" placeholder="Your link">
+				  </div>
+			  </p>
+			  <p id="urlerrorcontainer"></p>
+			  <p>Make it <button type="button" id="shorturl_btn" data-loading-text="Loading..." class="btn btn-primary btn-lg" >shorter!</button></p>
+			  <p id="servermessagescontainer"></p>
+			</div>
+		  </div>
+		</div>
+		
+	</body>
 </html>
---
+++ app/myUrlJ/myUrlUI/src/main/webapp/js/home.js
@@ -0,0 +1,106 @@
+$(document).ready(function() {
+	$("#shorturl_btn").click(function() {
+		var linkJQ = $("#link_input"),
+			url = linkJQ.val(),
+			shortBtnJQ = $(this);
+		if (url.length == 0) {
+			showLinkError(ERROR.URL.EMPTY, true);
+			return;
+		}
+		if (!re_weburl.test(url)) {
+			showLinkError(ERROR.URL.INVALID, true);
+			return;
+		}
+		$.ajax({
+			url:"", //TODO: fill ajax location
+			type:"post", //TODO: it will be a post?
+			data: {},//TODO: fill data
+			beforeSend: function(){
+				shortBtnJQ.button("loading");
+				linkJQ.attr("disabled", "disabled");
+			},
+			success: function(response){
+				response = $.trim(response);
+				var jsonRes = $.parseJSON(response);
+				if(jsonRes.s){
+					linkJQ.val("");
+				}
+				showServerMessage(jsonRes.s, jsonRes.m, jsonRes.l);
+			},
+			complete: function(){
+				shortBtnJQ.button("reset");
+				linkJQ.removeAttr("disabled");
+			}
+			
+		});
+		
+	});
+	
+	$("#link_input").keypress(function(){
+		clearUrlErrors();
+	});
+
+	function showLinkError(error, clearOther){
+		var message = null;
+		switch (error) {
+			case ERROR.URL.EMPTY:
+				message = "Please enter your link first!";
+				break;
+			case ERROR.URL.INVALID:
+				message = "Please enter a valid link!";
+				break;
+	
+			default:
+				message = null;
+				break;
+			}
+		var errorContainer = $("#urlerrorcontainer");
+		if(clearOther){
+			clearUrlErrors();
+		}
+		if(null != message){
+			errorContainer.append(
+					'<div role="alert" class="alert alert-danger alert-dismissible fade in">' +
+				      '<button aria-label="Close" data-dismiss="alert" class="close" type="button"><span aria-hidden="true">×</span></button>' +
+				      '<strong>Link error. </strong> ' + message +
+				    '</div>');
+		}
+	}
+	
+	function clearUrlErrors(){
+		var errorContainer = $("#urlerrorcontainer");
+		errorContainer.html("");
+	}
+	
+	function showServerMessage(success, message, link){
+		$("#servermessagescontainer").html(
+				'<div role="alert" class="alert alert-' + (success ? "success" : "danger") + ' alert-dismissible fade in">' +
+			      '<button aria-label="Close" data-dismiss="alert" class="close" type="button"><span aria-hidden="true">×</span></button>' +
+			      message +
+				      '<p>' +(success ? 
+				    		  		'<button class="btn btn-default" id="copylinkBtn">Copy μUrl link</button>'
+				      		   : '') +
+		      		 '</p>' +
+			    '</div>');
+	}
+
+});
+
+var ERROR = {};
+	ERROR.URL = {},
+	ERROR.URL.EMPTY = 0,
+	ERROR.URL.INVALID = 1;
+	
+
+var re_weburl = new RegExp(
+		"^" + "(?:(?:https?|http)://)?"
+		+ "(?:" + "(?!(?:10|127)(?:\\.\\d{1,3}){3})"
+		+ "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})"
+		+ "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})"
+		+ "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])"
+		+ "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}"
+		+ "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" + "|"
+		+ "(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)"
+		+ "(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*"
+		+ "(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))" + ")" + "(?::\\d{2,5})?"
+		+ "(?:/\\S*)?" + "$", "i");
